4bf48c027e82c8fb9a6dadc1cf6569cd370d7d8e
LYEmerald <qemerald2009@163.com>
2024-12-21 12:21:07 +0800

    新增Custom显示模式

--- src/main/java/net/endlight/festival/thread/PluginThread.java
+++ src/main/java/net/endlight/festival/thread/PluginThread.java
@@ -58,30 +58,155 @@ public class PluginThread extends Thread {
                     } else if (m >= 1) {
                         player.sendTip(tipMessageB);
                     }
-                    if (h == 0) {
-                        if (m == 0) {
-                            if (s <= 30) {
-                                if (s > 20) {
-                                    player.sendTitle(config.getString("Title.A").replaceAll("@second", String.valueOf(s)),
-                                            config.getString("SubTitle.A"), 0, 40, 0);
-                                } else if (s > 10) {
-                                    player.sendTitle(config.getString("Title.B").replaceAll("@second", String.valueOf(s)),
-                                            config.getString("SubTitle.B"), 0, 40, 0);
-                                } else {
-                                    player.sendTitle(config.getString("Title.C").replaceAll("@second", String.valueOf(s)),
-                                            config.getString("SubTitle.C"), 0, 40, 0);
-                                }
-                                Utils.playSound(player, Sound.NOTE_PLING, 0.840896F);
-                                Utils.playSound(player, Sound.NOTE_CHIME, 0.840896F);
+                    if (h == 0 && m == 0 && s <= 30) {
+                        if (config.getBoolean("SimpleMode")){
+                            if (s > 20) {
+                                player.sendTitle(config.getString("Simple_Title.A").replaceAll("@second", String.valueOf(s)),
+                                        config.getString("Simple_SubTitle.A"), 0, 40, 0);
+                            } else if (s > 10) {
+                                player.sendTitle(config.getString("Simple_Title.B").replaceAll("@second", String.valueOf(s)),
+                                        config.getString("Simple_SubTitle.B"), 0, 40, 0);
+                            } else {
+                                player.sendTitle(config.getString("Simple_Title.C").replaceAll("@second", String.valueOf(s)),
+                                        config.getString("Simple_Simple_SubTitle.C"), 0, 40, 0);
+                            }
+                        } else {
+                            switch ((int) s){
+                                case 30:
+                                    player.sendTitle(config.getString("Custom_Title.30s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.30s"), 0, 40, 0);
+                                    break;
+                                case 29:
+                                    player.sendTitle(config.getString("Custom_Title.29s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.29s"), 0, 40, 0);
+                                    break;
+                                case 28:
+                                    player.sendTitle(config.getString("Custom_Title.28s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.28s"), 0, 40, 0);
+                                    break;
+                                case 27:
+                                    player.sendTitle(config.getString("Custom_Title.27s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.27s"), 0, 40, 0);
+                                    break;
+                                case 26:
+                                    player.sendTitle(config.getString("Custom_Title.26s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.26s"), 0, 40, 0);
+                                    break;
+                                case 25:
+                                    player.sendTitle(config.getString("Custom_Title.25s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.25s"), 0, 40, 0);
+                                    break;
+                                case 24:
+                                    player.sendTitle(config.getString("Custom_Title.24s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.24s"), 0, 40, 0);
+                                    break;
+                                case 23:
+                                    player.sendTitle(config.getString("Custom_Title.23s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.23s"), 0, 40, 0);
+                                    break;
+                                case 22:
+                                    player.sendTitle(config.getString("Custom_Title.22s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.22s"), 0, 40, 0);
+                                    break;
+                                case 21:
+                                    player.sendTitle(config.getString("Custom_Title.21s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.21s"), 0, 40, 0);
+                                    break;
+                                case 20:
+                                    player.sendTitle(config.getString("Custom_Title.20s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.20s"), 0, 40, 0);
+                                    break;
+                                case 19:
+                                    player.sendTitle(config.getString("Custom_Title.19s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.19s"), 0, 40, 0);
+                                    break;
+                                case 18:
+                                    player.sendTitle(config.getString("Custom_Title.18s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.18s"), 0, 40, 0);
+                                    break;
+                                case 17:
+                                    player.sendTitle(config.getString("Custom_Title.17s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.17s"), 0, 40, 0);
+                                    break;
+                                case 16:
+                                    player.sendTitle(config.getString("Custom_Title.16s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.16s"), 0, 40, 0);
+                                    break;
+                                case 15:
+                                    player.sendTitle(config.getString("Custom_Title.15s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.15s"), 0, 40, 0);
+                                    break;
+                                case 14:
+                                    player.sendTitle(config.getString("Custom_Title.14s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.14s"), 0, 40, 0);
+                                    break;
+                                case 13:
+                                    player.sendTitle(config.getString("Custom_Title.13s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.13s"), 0, 40, 0);
+                                    break;
+                                case 12:
+                                    player.sendTitle(config.getString("Custom_Title.12s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.12s"), 0, 40, 0);
+                                    break;
+                                case 11:
+                                    player.sendTitle(config.getString("Custom_Title.11s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.11s"), 0, 40, 0);
+                                    break;
+                                case 10:
+                                    player.sendTitle(config.getString("Custom_Title.10s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.10s"), 0, 40, 0);
+                                    break;
+                                case 9:
+                                    player.sendTitle(config.getString("Custom_Title.9s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.9s"), 0, 40, 0);
+                                    break;
+                                case 8:
+                                    player.sendTitle(config.getString("Custom_Title.8s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.8s"), 0, 40, 0);
+                                    break;
+                                case 7:
+                                    player.sendTitle(config.getString("Custom_Title.7s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.7s"), 0, 40, 0);
+                                    break;
+                                case 6:
+                                    player.sendTitle(config.getString("Custom_Title.6s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.6s"), 0, 40, 0);
+                                    break;
+                                case 5:
+                                    player.sendTitle(config.getString("Custom_Title.5s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.5s"), 0, 40, 0);
+                                    break;
+                                case 4:
+                                    player.sendTitle(config.getString("Custom_Title.4s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.4s"), 0, 40, 0);
+                                    break;
+                                case 3:
+                                    player.sendTitle(config.getString("Custom_Title.3s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.3s"), 0, 40, 0);
+                                    break;
+                                case 2:
+                                    player.sendTitle(config.getString("Custom_Title.2s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.2s"), 0, 40, 0);
+                                    break;
+                                case 1:
+                                    player.sendTitle(config.getString("Custom_Title.1s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.1s"), 0, 40, 0);
+                                    break;
+                                case 0:
+                                    player.sendTitle(config.getString("Custom_Title.0s").replaceAll("@second", String.valueOf(s)),
+                                            config.getString("Custom_SubTitle.0s"), 0, 40, 0);
+                                    break;
                             }
                         }
+                        Utils.playSound(player, Sound.NOTE_PLING, 0.840896F);
+                        Utils.playSound(player, Sound.NOTE_CHIME, 0.840896F);
                     }
                 }
                 if (h == 0 && m == 0 && s == 0 ) {
                     Festival.getInstance().getServer().getScheduler().scheduleDelayedTask(Festival.getInstance(), () -> {
                         for (Player player : Festival.getInstance().getServer().getOnlinePlayers().values()) {
-                            player.sendTitle(config.getString("Title.Title"),
-                                    config.getString("SubTitle.SubTitle"), 10, 60, 10);
+                            player.sendTitle(config.getString("Final_Title.Title"),
+                                    config.getString("Final_Title.SubTitle"), 10, 60, 10);
                             // 发送标题
 
                             Utils.spawnFirework(player.getPosition(),player.getLevel());
